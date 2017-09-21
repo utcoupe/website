5b82b0ae67222acf79961db1ec0da228701ae59d
Unknown <mewenmichel@yahoo.fr>
2017-09-22 00:03:22 +0200

    Pictures are coming...

--- assets/js/custom_pictures.js
+++ assets/js/custom_pictures.js
@@ -1,26 +1,63 @@
 (function($){
   $(function(){
+    var currentArticle = null;
+    var currentArticleIdx = -1;
+    var allArticles = $("#main").find("article");
+    var overlayOn = false;
+
+    function resizePictureDiv() {
+        $("#image").width("auto");
+        $("#image").width($("#image img").width());
+    }
+
     function closeBigPicture () {
         $("#overlay").fadeOut();
+        overlayOn = false;
         return false;
     }
 
-    function openBigPicture (evt) {
-        $("#overlay").fadeIn();
+    function openBigPicture () {
+        overlayOn = true;
+        $("#image").width("auto");
+        $("#overlay").fadeIn(resizePictureDiv);
     }
 
     function updateBigPicture (article) {
-        // TODO
+        currentArticle = article;
+        $("#big-picture").find("img").attr("src", $(article).find("img").first().attr("src"));
+        $("#big-picture").children("h3").text($(article).find("h3").first().text());
+        $("#big-picture").children("p").text($(article).find("p").first().text());
+        resizePictureDiv();
+
+        if (currentArticleIdx == -1) {
+            // Find article idx
+            let i;
+            var allArticles = $("#main").find("article");
+            for (i=0; i < allArticles.length; i++) {
+                if (allArticles[i] == currentArticle) {
+                    break;
+                }
+            }
+            if (i == allArticles.length)
+                i = 0;
+            currentArticleIdx = i;
+        }
     }
 
-    function previousBigPicture (evt) {
-        // TODO
-        // updateBigPicture (article);
+    function previousBigPicture () {
+        currentArticleIdx--;
+        if(currentArticleIdx == -1)
+            currentArticleIdx = allArticles.length - 1;
+        updateBigPicture(allArticles[currentArticleIdx]);
+        return false;
     }
 
-    function nextBigPicture (evt) {
-        // TODO
-        // updateBigPicture (article);
+    function nextBigPicture () {
+        currentArticleIdx++;
+        if(currentArticleIdx == allArticles.length)
+            currentArticleIdx = 0;
+        updateBigPicture(allArticles[currentArticleIdx]);
+        return false;
     }
 
     $("#close-big-picture").click(closeBigPicture);
@@ -32,11 +69,31 @@
     });
 
     $("a.open-big-picture").click(function (evt) {
-        updateBigPicture(evt);
-        openBigPicture(evt);
+        article = $(evt.currentTarget).parents("article")[0]
+        updateBigPicture(article);
+        openBigPicture();
         return false;
     });
 
+    $("#overlay").css("display", "flex").hide();
+
+    $(window).resize(resizePictureDiv);
+
+    $( "html" ).keypress(function( event ) {
+        if (!overlayOn) {
+            return;
+        }
+
+        if ( event.which == 13 ) {
+            event.preventDefault();
+        }
+
+        if (event.key=="ArrowRight") {
+            nextBigPicture();
+        } else if (event.key=="ArrowLeft") {
+            previousBigPicture();
+        }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
